6c64889b3c91b4abe7dc38831d90f5cad84891b0
krudda <krudda@yandex.ru>
2025-02-24 08:55:28 +0300

    Fix FlatList for gallery

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -160,10 +160,11 @@ const Property = () => {
             <View className="mt-7">
               <Text className="text-black-300 text-xl font-rubik-bold">Gallery</Text>
               <FlatList
-                contentContainerStyle={{ paddingRight: 20 }}
-                data={property?.gallery}
-                keyExtractor={(item) => item.$id}
                 horizontal
+                data={property?.gallery}
+                contentContainerStyle={{ paddingRight: 20 }}
+                keyExtractor={(item) => item.$id}
+                bounces={false}
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item }) => (
                   <Image source={{ uri: item.image }} className="size-40 rounded-xl" />
